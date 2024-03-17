80304adf46260a3f55ee7a269d424b50354b285d
anderle02 <anderle.minecraft@gmail.com>
2024-03-17 16:46:51 +0100

    finished rendering the container overlay

--- src/main/java/dev/anderle/attributemod/features/ContainerValue.java
+++ src/main/java/dev/anderle/attributemod/features/ContainerValue.java
@@ -2,44 +2,47 @@ package dev.anderle.attributemod.features;
 
 import dev.anderle.attributemod.utils.Helper;
 import dev.anderle.attributemod.utils.ItemWithAttributes;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.inventory.GuiChest;
 import net.minecraft.inventory.Slot;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraftforge.client.event.GuiOpenEvent;
+import net.minecraft.util.EnumChatFormatting;
 import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class ContainerValue {
-
-    /**
-     * Save here when this ContainerValue overlay was rendered the last time.
-     */
-    private long overlayLastRenderedAt = 0;
-    /**
-     * Render the overlay only once every x milliseconds to reduce lag when in a GUI.
-     */
-    private final int interval = 1000;
-
-    @SubscribeEvent // render overlay when a new gui was opened
-    public void onOpenGui(GuiOpenEvent e) {
-        this.overlayLastRenderedAt = 0;
-    }
+    // These values are hardcoded in Minecraft, so I'll just hardcode them here too.
+    public static final int CHEST_GUI_WIDTH = 176;
+    public static final int CHEST_GUI_HEIGHT = 220;
 
     @SubscribeEvent
     public void onDrawGuiBackground(GuiScreenEvent.BackgroundDrawnEvent e) {
         // Guis like main menu, inventory and anything else that's not a chest.
         if(!(e.gui instanceof GuiChest)) return;
 
-        // Skip rendering this time if it was rendered shortly before.
-        long currentTime = System.currentTimeMillis();
-        if(currentTime < this.overlayLastRenderedAt + this.interval) return;
-        this.overlayLastRenderedAt = currentTime;
-
-        // Find and save prices of all items with attributes in that GUI.
+        // Check all chest slots for items with attributes and organize them.
         List<Slot> allSlots = ((GuiChest) e.gui).inventorySlots.inventorySlots;
-        List<Slot> chestSlots = allSlots.subList(0, allSlots.size() - 36);
-        for(Slot slot : chestSlots) {
+        ItemWithAttributes[] items = this.getValidItems(allSlots.subList(0, allSlots.size() - 36));
+        if(items.length == 0) return;
+
+        // Sort items by estimated price.
+        this.sortItems(items);
+
+        // Render overlay.
+        this.renderItemInformation((GuiChest) e.gui, items);
+    }
+
+    private ItemWithAttributes[] getValidItems(List<Slot> slotsToCheck) {
+        List<ItemWithAttributes> items = new ArrayList<ItemWithAttributes>();
+
+        for(Slot slot : slotsToCheck) {
             if(!slot.getHasStack()) continue;
             NBTTagCompound extra = slot.getStack().serializeNBT()
                     .getCompoundTag("tag").getCompoundTag("ExtraAttributes");
@@ -53,11 +56,67 @@ public class ContainerValue {
             for(String attribute : attributeCompound.getKeySet()) {
                 item.addAttribute(Helper.formatAttribute(attribute), attributeCompound.getInteger(attribute));
             }
-
-            ItemWithAttributes.Evaluation price = item.getDetailedPrice();
-            System.out.println(price.getEstimate());
+            items.add(item);
         }
 
-        // Render Overlay
+        return items.toArray(new ItemWithAttributes[0]);
+    }
+
+    private void sortItems(ItemWithAttributes[] items) {
+        Arrays.sort(items, new Comparator<ItemWithAttributes>() {
+            @Override
+            public int compare(ItemWithAttributes i1, ItemWithAttributes i2) {
+                int price1 = i1.getDetailedPrice().getEstimate();
+                int price2 = i2.getDetailedPrice().getEstimate();
+                if(price1 < price2) return 1;
+                if(price1 > price2) return -1;
+                return 0;
+            }
+        });
+    }
+
+    private void renderItemInformation(GuiChest gui, ItemWithAttributes[] items) {
+        FontRenderer renderer = Minecraft.getMinecraft().fontRendererObj;
+        Point overlayPos = new Point(
+                (gui.width + CHEST_GUI_WIDTH) / 2,
+                (gui.height - CHEST_GUI_HEIGHT) / 2
+        );
+        int maxNameLength = this.getMaxItemNameLength(items);
+        for(int i = 0; i < items.length; i++) {
+            ItemWithAttributes item = items[i];
+            ItemWithAttributes.Evaluation price = item.getDetailedPrice();
+            String[] attributeNames = item.getAttributeNames();
+            Integer[] attributeLevels = item.getAttributeLevels();
+            String itemName = item.getDisplayName();
+
+            StringBuilder alignmentString = new StringBuilder(" ");
+            for(int j = 0; j < maxNameLength - itemName.length(); j++) alignmentString.append(" ");
+
+            StringBuilder displayString = new StringBuilder()
+            .append(EnumChatFormatting.GOLD).append(Helper.format(price.getEstimate()))
+            .append(" ").append(EnumChatFormatting.YELLOW).append(itemName)
+            .append(alignmentString).append(EnumChatFormatting.AQUA).append(attributeNames[0])
+            .append(" ").append(attributeLevels[0]);
+
+            if(attributeNames.length == 2) displayString
+                .append(EnumChatFormatting.YELLOW)
+                .append(", ").append(EnumChatFormatting.AQUA).append(attributeNames[1])
+                .append(" ").append(attributeLevels[1]);
+
+            gui.drawString(
+                    renderer, displayString.toString(),
+                    overlayPos.x, overlayPos.y + i * (renderer.FONT_HEIGHT + 1),
+                    0xffffffff
+            );
+        }
+    }
+
+    private int getMaxItemNameLength(ItemWithAttributes[] items) {
+        int max = 0;
+        for(ItemWithAttributes item : items) {
+            int length = item.getDisplayName().length();
+            if(length > max) max = length;
+        }
+        return max;
     }
 }
--- src/main/java/dev/anderle/attributemod/utils/ItemWithAttributes.java
+++ src/main/java/dev/anderle/attributemod/utils/ItemWithAttributes.java
@@ -8,26 +8,39 @@ import java.util.List;
 import java.util.Map;
 
 public class ItemWithAttributes {
-    HashMap<String, Integer> attributes = new HashMap<String, Integer>();
-    String id;
+    private final HashMap<String, Integer> attributes = new HashMap<String, Integer>();
+    private final String id;
+    private final String displayName;
+    private Evaluation price;
 
     public ItemWithAttributes(String itemId) {
         this.id = itemId;
+        this.displayName = Helper.itemIdToName(itemId);
     }
 
     public void addAttribute(String name, int level) {
         this.attributes.put(name, level);
     }
 
+    public String[] getAttributeNames() {
+        return this.attributes.keySet().toArray(new String[0]);
+    }
+
+    public Integer[] getAttributeLevels() {
+        return this.attributes.values().toArray(new Integer[0]);
+    }
+
     public Evaluation getDetailedPrice() {
+        if(this.price != null) return this.price; // so it doesn't calculate more than 1 time
+
         int lbin = Main.api.getLbin(this.id);
         int estimate = lbin;
 
         HashMap<String, Integer> singlePrices = new HashMap<String, Integer>();
-        for(Map.Entry attribute : this.attributes.entrySet()) {
-            double singlePrice = Math.pow(2, Integer.parseInt(attribute.getValue().toString()) - 1)
-                    * Main.api.getAttributePrice(this.id, attribute.getKey().toString());
-            singlePrices.put(attribute.getKey().toString(), (int) singlePrice);
+        for(Map.Entry<String, Integer> attribute : this.attributes.entrySet()) {
+            double singlePrice = Math.pow(2, attribute.getValue() - 1)
+                    * Main.api.getAttributePrice(this.id, attribute.getKey());
+            singlePrices.put(attribute.getKey(), (int) singlePrice);
             estimate += singlePrice - lbin;
         }
 
@@ -39,7 +52,13 @@ public class ItemWithAttributes {
         }
 
         if(estimate < lbin) estimate = lbin;
-        return new Evaluation(singlePrices, combinationPrice, estimate);
+
+        this.price = new Evaluation(singlePrices, combinationPrice, estimate);
+        return this.price;
+    }
+
+    public String getDisplayName() {
+        return this.displayName;
     }
 
     public static class Evaluation {
