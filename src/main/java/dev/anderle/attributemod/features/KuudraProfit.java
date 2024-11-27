6004e7f692d9414a094dcc5809cf3a3d0ac44c20
= <anderle.minecraft@gmail.com>
2024-11-28 00:05:01 +0100

    send correct kuudra chest data

--- src/main/java/dev/anderle/attributemod/features/KuudraProfit.java
+++ src/main/java/dev/anderle/attributemod/features/KuudraProfit.java
@@ -6,14 +6,18 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
 import dev.anderle.attributemod.Main;
 import dev.anderle.attributemod.api.PriceApi;
+import dev.anderle.attributemod.utils.Helper;
 import dev.anderle.attributemod.utils.ItemWithAttributes;
 import net.minecraft.inventory.ContainerChest;
+import net.minecraft.inventory.Slot;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.JsonUtils;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class KuudraProfit {
-    private ItemWithAttributes[] chestItems;
+    private JsonArray chestItems;
 
     public KuudraProfit() {
 
@@ -21,6 +25,7 @@ public class KuudraProfit {
 
     // I would get all items instantly, but I think Hypixel sends those later.
     public void onGuiOpen(ContainerChest chest) {
+        chestItems = null;
         new Thread(() -> {
             try {
                 Thread.sleep(100);
@@ -30,12 +35,23 @@ public class KuudraProfit {
     }
 
     public void getProfitData(ContainerChest chest) {
-        chestItems = ItemWithAttributes.getValidItems(chest.inventorySlots.subList(0, chest.inventorySlots.size() - 36));
-        JsonArray array = new JsonArray();
-        for(ItemWithAttributes item : chestItems) {
-            array.add(item.getNBTAsJson());
+        List<Slot> slots = chest.inventorySlots.subList(0, chest.inventorySlots.size() - 36);
+        chestItems = new JsonArray();
+
+        for(Slot slot : slots) {
+            if(!slot.getHasStack()) continue;
+
+            JsonObject nbt = Helper.convertNBTToJson(slot.getStack().writeToNBT(new NBTTagCompound()));
+            String id = nbt.get("id").getAsString();
+
+            if(id.equals("minecraft:stained_glass_pane") || id.equals("minecraft:chest") || id.equals("minecraft:barrier")) continue;
+            if(Helper.getHypixelId(nbt).equals("KUUDRA_TEETH")) nbt.add("amount", new JsonPrimitive(slot.getStack().stackSize));
+
+            chestItems.add(nbt);
+            System.out.println("Slot " + slot.getSlotIndex() + ": " + nbt);
         }
-        Main.api.sendPostRequest("/kuudrachest", "", array.toString(), new PriceApi.ResponseCallback() {
+
+        Main.api.sendPostRequest("/kuudrachest", "", chestItems.toString(), new PriceApi.ResponseCallback() {
             @Override
             public void onResponse(String a) {
                 System.out.println(a);
