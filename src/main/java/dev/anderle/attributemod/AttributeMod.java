6e62289ca86dfad51b8a0287790690f3dcac128e
= <anderle.minecraft@gmail.com>
2025-06-04 16:50:12 +0200

    set mod metadata and added devauth

--- src/main/java/dev/anderle/attributemod/AttributeMod.java
+++ src/main/java/dev/anderle/attributemod/AttributeMod.java
@@ -1,16 +1,16 @@
 package dev.anderle.attributemod;
 
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ClientModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class AttributeMod implements ModInitializer {
+public class AttributeMod implements ClientModInitializer {
 	public static final String MOD_ID = "attributemod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
-	public void onInitialize() {
+	public void onInitializeClient() {
 		LOGGER.info("Hello Fabric world!");
 	}
 }
